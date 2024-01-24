0ea686d56e63f651d3cb79bdb10ba4e40436bfc7
Kevin S Kostage <73184531+Keko787@users.noreply.github.com>
2024-01-23 22:07:50 -0500

    Delete renderer.js

--- renderer.js
+++
@@ -1,9 +0,0 @@
-// renderer.js
-document.getElementById('loadButton').addEventListener('click', () => { // event listener on load button
-    fetch('http://localhost:5000/Kevin') // uses the route flask is using for the get_text
-        .then(response => response.text()) // gets the text from flask in form of response
-        .then(text => {
-            document.getElementById('displayText').innerText = text;  // puts the text into displayText variable in index.html
-        })
-        .catch(err => console.error('Error fetching text:', err));  // error handling
-});
