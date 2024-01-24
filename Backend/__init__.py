e462826f4c25f4272c4af7922dd5bf2278d85f2f
Kevin S Kostage <73184531+Keko787@users.noreply.github.com>
2024-01-23 20:40:14 -0500

    Delete Backend directory
    cleaning up to just get debugger

--- Backend/__init__.py
+++
@@ -1,7 +0,0 @@
-from .Facade import Facade
-from .Keywords import Keywords
-from .ScraperPrototype import ScraperPrototype
-
-Facade = Facade
-Keywords = Keywords
-ScraperPrototype = ScraperPrototype
