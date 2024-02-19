0a7aa8ceefe0d0153c64591829f6b5d49d4a9f49
Kevin Kostage <kskostage9457@eagle.fgcu.edu>
2024-02-19 06:18:10 -0500

    planning for the append and diagram making features

---
+++ Backend/makeDiagrams.py
@@ -0,0 +1,30 @@
+
+# ---- loading ----
+
+# ---- preprocessing ----
+
+# ---- coorelations ----
+
+# location vs timeline
+
+# location vs payment
+
+# location vs social media
+
+# keyword frequency
+
+# keyword vs location
+
+# keyword vs timeline
+
+#  # posts vs timeline
+
+#  # posts vs region
+
+#  # of keywords found vs region
+
+#  # of keywords found vs timeline
+
+
+# ---- saving ----
+
