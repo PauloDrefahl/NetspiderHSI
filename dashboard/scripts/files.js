1d89fd42408ce2a407b2e9c5fce8b38ca9ecead5
Zach <zesutton2619@eagle.fgcu.edu>
2024-04-17 01:42:06 -0400

    Fixed font in result manager

--- dashboard/scripts/files.js
+++ dashboard/scripts/files.js
@@ -217,7 +217,6 @@ document.addEventListener("DOMContentLoaded", function () {
             folderList.forEach(item => {
                 const listItem = document.createElement('li');
                 listItem.textContent = item;  // adding text conte
-                item.style.fontSize = "20px";
                 listElement.appendChild(listItem);
 
                 listItem.addEventListener('click', function () {
