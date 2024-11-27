2131764622c556298a2c7159f346dee618958776
Daniel Kareh <33532334+dkareh@users.noreply.github.com>
2024-11-27 13:17:38 -0500

    Define common data types and domains in the schema
    The `exception` clause should be removed before using the schema in
    production, but in the meantime, it makes development easier.

--- server/Backend/schema/v00.sql
+++ server/Backend/schema/v00.sql
@@ -4,4 +4,18 @@
 -- all data types, domains, and relations every time the scraper is run:
 
 /*
+drop type if exists sex cascade;
+drop domain if exists url, phone_number, email_address cascade;
 */
+
+do $$
+    begin
+        create type sex as enum ('Male', 'Female');
+        create domain url as varchar(256) check (value <> '');
+        create domain phone_number as varchar(64) check (value <> '');
+        create domain email_address as varchar(128) check (value <> '');
+    exception
+        when duplicate_object then
+            null;
+    end;
+$$;
