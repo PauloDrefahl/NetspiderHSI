74a50df40bbc924787fe403afa1ba5a894e72e21
Daniel Kareh <33532334+dkareh@users.noreply.github.com>
2024-12-22 00:17:51 -0500

    Automatically create `netspider` database

--- server/Backend/database.py
+++ server/Backend/database.py
@@ -1,7 +1,11 @@
+import contextlib
 from importlib import resources
 from typing import LiteralString, cast
 
 import psycopg
+from psycopg import sql
+
+MAIN_DATABASE_NAME = "netspider"
 
 
 def open() -> psycopg.Connection:
@@ -15,15 +19,37 @@ def open() -> psycopg.Connection:
 
 
 def _connect() -> psycopg.Connection:
+    try:
+        return _connect_to(MAIN_DATABASE_NAME)
+    except psycopg.OperationalError:
+        # NOTE: An operational error can be caused by many things, but there's
+        # no way to determine the specific error except clumsily checking the
+        # error message. Fortunately, attempting to create the main database in
+        # response to ANY operational error causes no issues.
+        _create_main_database()
+        return _connect_to(MAIN_DATABASE_NAME)
+
+
+def _create_main_database() -> None:
+    with _connect_to("postgres") as connection, connection.cursor() as cursor:
+        name = sql.Identifier(MAIN_DATABASE_NAME)
+        command = sql.SQL("create database {};").format(name)
+        with contextlib.suppress(psycopg.errors.DuplicateDatabase):
+            cursor.execute(command)
+
+
+def _connect_to(database_name: str) -> psycopg.Connection:
     return psycopg.connect(
         autocommit=True,
         host="localhost",
         port=5432,
-        dbname="netspider",
+        dbname=database_name,
         user="postgres",
         # NOTE: NetSpider is intended to be run locally, so we don't need to
         # worry about authentication.
         password="password",  # noqa: S106
+        client_encoding="UTF8",
+        application_name="NetSpider",
     )
 
 
