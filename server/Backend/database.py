f68a7a38fee7ad93627c32a757eadf34ddde50f2
Daniel Kareh <33532334+dkareh@users.noreply.github.com>
2024-12-22 12:41:46 -0500

    Implement schema migration

--- server/Backend/database.py
+++ server/Backend/database.py
@@ -1,5 +1,11 @@
+__all__ = ["SchemaError", "open"]
+
 import contextlib
+import re
+from dataclasses import dataclass
+from datetime import UTC, datetime
 from importlib import resources
+from importlib.resources.abc import Traversable
 from typing import LiteralString, cast
 
 import psycopg
@@ -8,14 +14,21 @@ from psycopg import sql
 MAIN_DATABASE_NAME = "netspider"
 
 
+@dataclass
+class SchemaVersion:
+    number: int
+    resource: Traversable
+
+
 def open() -> psycopg.Connection:
     connection = _connect()
     try:
-        _prepare(connection)
-        return connection
-    except Exception:
+        _migrate(connection)
+    except:
         connection.close()
         raise
+    else:
+        return connection
 
 
 def _connect() -> psycopg.Connection:
@@ -53,10 +66,93 @@ def _connect_to(database_name: str) -> psycopg.Connection:
     )
 
 
-def _prepare(connection: psycopg.Connection) -> None:
-    with connection.cursor() as cursor:
-        schema_v00_resource = resources.files() / "schema" / "v00.sql"
-        schema_v00 = schema_v00_resource.read_text(encoding="utf-8")
-        # WARNING: This type cast bypasses the SQL injection prevention, but
-        # `schema_v00` comes from a trusted source (us!), so it's okay.
-        cursor.execute(cast(LiteralString, schema_v00))
+def _migrate(connection: psycopg.Connection) -> None:
+    versions = _get_schema_versions()
+    with connection.cursor() as cursor, connection.transaction():
+        current_version = _get_current_version(connection, cursor)
+        next_version = 0 if current_version is None else current_version + 1
+        for version in versions[next_version:]:
+            _migrate_to(cursor, version)
+
+
+def _migrate_to(cursor: psycopg.Cursor, version: SchemaVersion) -> None:
+    commands = version.resource.read_text(encoding="utf-8")
+    execution_begin_timestamp = datetime.now(UTC)
+    # WARNING: This type cast bypasses the SQL injection prevention, but
+    # `schema_v00` comes from a trusted source (us!), so it's okay.
+    cursor.execute(cast(LiteralString, commands))
+    cursor.execute(
+        """
+        insert into schema_versions (version_number, migrated_on, execution_time)
+        values (%s, statement_timestamp(), statement_timestamp() - %s);
+        """,
+        (version.number, execution_begin_timestamp),
+    )
+
+
+def _get_current_version(
+    connection: psycopg.Connection,
+    cursor: psycopg.Cursor,
+) -> int | None:
+    try:
+        with connection.transaction():
+            cursor.execute("""
+                create table schema_versions (
+                    primary key (version_number),
+                    version_number integer not null,
+                    migrated_on timestamptz not null,
+                    execution_time interval second not null
+                    check (version_number >= 0),
+                    check (execution_time >= interval '0' second)
+                );
+            """)
+    except psycopg.errors.DuplicateTable:
+        pass
+
+    cursor.execute("select max(version_number) from schema_versions;")
+    record = cursor.fetchone()
+    assert record is not None
+    if record[0] is None:
+        return None
+    assert isinstance(record[0], int)
+    return record[0]
+
+
+_VERSION_DIGITS: int = 2
+_VERSION_REGEX = re.compile(rf"v(?P<number>[0-9]{{{_VERSION_DIGITS}}})\.sql")
+
+
+class SchemaError(Exception):
+    pass
+
+
+def _get_schema_versions() -> list[SchemaVersion]:
+    schema_container = resources.files() / "schema"
+    schema = [*schema_container.iterdir()]
+    # Parse version numbers and ignore extraneous resources.
+    versions: list[SchemaVersion] = []
+    for resource in schema:
+        match = _VERSION_REGEX.fullmatch(resource.name)
+        if not match:
+            continue
+        number = int(match.group("number"))
+        versions.append(SchemaVersion(number, resource))
+    # Put versions in ascending order.
+    versions.sort(key=lambda version: version.number)
+    # Check for missing versions.
+    if not versions:
+        message = "No versions were found."
+        raise SchemaError(message)
+    for i, version in enumerate(versions):
+        if i != version.number:
+            message = f"Version {i} is missing."
+            error = SchemaError(message)
+            error.add_note(
+                f"Version {version.number} was found, "
+                + "but version numbers begin at 0 and cannot be skipped.",
+            )
+            raise error
+        if version.resource.is_dir():
+            message = f"Resource '{version.resource.name}' is a directory."
+            raise SchemaError(message)
+    return versions
