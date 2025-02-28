34bdd83530767a99d9dfd3c60f79a6152cff0d3d
Daniel Kareh <33532334+dkareh@users.noreply.github.com>
2025-02-28 18:48:53 -0500

    Automatically create the NetSpider database

--- server/Backend/database.py
+++ server/Backend/database.py
@@ -1,14 +1,32 @@
+from contextlib import suppress
 from importlib import resources
 from typing import LiteralString, NamedTuple, cast
 
 import psycopg
+from psycopg import sql
 from psycopg.rows import namedtuple_row
 
 from . import schema
 
+_DATABASE_NAME = "netspider"
+
 
 def open() -> psycopg.Connection[NamedTuple]:
-    connection = _connect()
+    try:
+        # Eagerly attempt to connect to the NetSpider database.
+        connection = _connect()
+    except psycopg.errors.ConnectionTimeout:
+        # `ConnectionTimeout` is a subclass of `OperationalError`, but we can't
+        # resolve `ConnectionTimeout` errors, so we re-raise them instead.
+        raise
+    except psycopg.OperationalError:
+        # Psycopg doesn't assign an error code for attempting to connect to a
+        # nonexistent database, so we try to create the database and re-connect
+        # in response to *any* operational error.
+        _create_database()
+        connection = _connect()
+
+    # Prepare the database before returning the connection object.
     try:
         _prepare(connection)
     except:
@@ -18,18 +36,31 @@ def open() -> psycopg.Connection[NamedTuple]:
         return connection
 
 
-def _connect() -> psycopg.Connection[NamedTuple]:
+def _create_database() -> None:
+    # According to PostgreSQL's documentation, we should connect to the
+    # `postgres` database to execute the `CREATE DATABASE` command.
+    # Source: https://www.postgresql.org/docs/17/manage-ag-createdb.html
+    with _connect(dbname="postgres") as connection:
+        name = sql.Identifier(_DATABASE_NAME)
+        command = sql.SQL("create database {} template template0 encoding 'UTF8';")
+        with suppress(psycopg.errors.DuplicateDatabase):
+            connection.execute(command.format(name))
+
+
+def _connect(*, dbname: str = _DATABASE_NAME) -> psycopg.Connection[NamedTuple]:
     return psycopg.Connection[NamedTuple].connect(
         autocommit=True,
         row_factory=namedtuple_row,
         host="localhost",
         port=5432,
-        dbname="netspider",
+        dbname=dbname,
         user="postgres",
         # WARNING: To save users from frequently entering passwords, we use
         # a hardcoded password. NetSpider is intended to run *locally*, so we
         # don't really need proper authentication or secrets management.
         password="password",  # noqa: S106
+        client_encoding="UTF8",
+        application_name="NetSpider",
     )
 
 
