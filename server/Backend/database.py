22639418dec5bd835e5f613e04ab5455c751401c
Daniel Kareh <33532334+dkareh@users.noreply.github.com>
2025-03-15 21:08:56 -0400

    Document `database` and `ScraperPrototype`

--- server/Backend/database.py
+++ server/Backend/database.py
@@ -1,3 +1,7 @@
+"""Set up and connect to the database."""
+
+__all__ = ["connect"]
+
 from importlib import resources
 from typing import LiteralString, NamedTuple, cast
 
@@ -8,6 +12,7 @@ from . import schema
 
 
 def connect() -> psycopg.Connection[NamedTuple]:
+    """Connect to the database."""
     connection = _open_connection()
     try:
         _set_up(connection)
@@ -19,6 +24,7 @@ def connect() -> psycopg.Connection[NamedTuple]:
 
 
 def _open_connection() -> psycopg.Connection[NamedTuple]:
+    """Open a connection to a database using preset parameters."""
     return psycopg.Connection[NamedTuple].connect(
         autocommit=True,
         row_factory=namedtuple_row,
@@ -34,6 +40,7 @@ def _open_connection() -> psycopg.Connection[NamedTuple]:
 
 
 def _set_up(connection: psycopg.Connection[NamedTuple]) -> None:
+    """Create data types and objects according to the schema."""
     with connection.cursor() as cursor:
         schema_v00_resource = resources.files(schema) / "v00.sql"
         schema_v00 = schema_v00_resource.read_text(encoding="utf-8")
