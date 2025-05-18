35a2529b907d54db7ae0bfbce168573c84916d0e
Daniel Kareh <33532334+dkareh@users.noreply.github.com>
2025-05-18 11:28:32 -0400

    Remove window creation delay

--- client/index.js
+++ client/index.js
@@ -98,12 +98,7 @@ const createWindow = () => {
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
-app.on('ready', () => {
-    // Add a delay before creating the window
-    setTimeout(() => {
-        createWindow();
-    }, 1500); // 15000 milliseconds = 15 seconds
-});
+app.whenReady().then(createWindow);
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
